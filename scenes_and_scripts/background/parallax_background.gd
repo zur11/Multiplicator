0c6e47219f09ac755ba2263cc831ab49efad7f6f
zur11 <cibka11@gmail.com>
2022-04-08 12:19:05 -0500

    First Commit - Basic Scenes Setup Done

---
+++ scenes_and_scripts/background/parallax_background.gd
@@ -0,0 +1,11 @@
+extends ParallaxBackground
+
+onready var fondo = get_node("ParallaxLayer")
+onready var fondo2 = get_node("ParallaxLayer2")
+
+func _ready():
+	pass 
+
+func _physics_process(_delta):
+	fondo.motion_offset.x += -.1
+	fondo2.motion_offset.x += -.08
